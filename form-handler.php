117e192139422037dd6fd56933fcc72ad22de326
Anton Suba <antonsuba@outlook.com>
2017-04-29 19:40:55 +0800

    Implement csrf token and recaptcha verification

---
+++ form-handler.php
@@ -0,0 +1,47 @@
+<?php
+
+session_start();
+generateToken();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if(!isset($_POST['token']) || !isset($_POST['g-recaptcha-response'])){
+        
+    }
+
+    $url = 'https://www.google.com/recaptcha/api/siteverify';
+    $secret = '6Le8jx4UAAAAAHi5D1WswDJ4dhES6JHwURLzY8h_';
+    if(verifyToken($_POST['token']) && verifyRecaptcha($url, $secret, $_POST['g-recaptcha-response'])){
+        
+    }
+}
+
+function generateToken(){
+    if(empty($_SESSION['token_key'])){
+        $_SESSION['token_key'] = bin2hex(random_bytes(32));
+    }
+    return $_SESSION['token_key'];
+}
+
+function verifyToken($token){
+    $val = hash_hmac('sha256', '/form-handler.php', $_SESSION['token_key']);
+    return hash_equals($val, $token);
+}
+
+function verifyRecaptcha($url, $secret, $response){
+    $data = array(
+        'secret' => $secret,
+        'response' => $response
+    );
+    $options = array(
+        'http' => array(
+            'method' => 'POST',
+            'content' => http_build_query($data)
+        )
+    );
+    $context = stream_context_create($options);
+    $result = json_decode(file_get_contents($url, false, $context));
+
+    return $result->success;
+}
+
+?>
--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+require_once('form-handler.php');
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -8,6 +12,7 @@
 
         <title>JFFC</title>
 
+        <!--Critical CSS-->
         <style type="text/css">
             @font-face {
               font-family: 'Work Sans';
@@ -502,15 +507,25 @@
             }
 
         </style>
+
+        <!--Fonts-->
         <link href="https://fonts.googleapis.com/css?family=Work+Sans:400,500" rel="preload" as="style" onload="this.rel='stylesheet'">
         <noscript><link href="https://fonts.googleapis.com/css?family=Work+Sans:400,500" rel="stylesheet"></noscript>
 
+        <!--CSS-->
         <link rel="preload" href="css/main.css" as="style" onload="this.rel='stylesheet'">
         <noscript><link rel="stylesheet" href="css/main.css"></noscript>
         <script>!function(e){"use strict";var t=function(t,n,r){function o(e){return i.body?e():void setTimeout(function(){o(e)})}function a(){d.addEventListener&&d.removeEventListener("load",a),d.media=r||"all"}var l,i=e.document,d=i.createElement("link");if(n)l=n;else{var s=(i.body||i.getElementsByTagName("head")[0]).childNodes;l=s[s.length-1]}var u=i.styleSheets;d.rel="stylesheet",d.href=t,d.media="only x",o(function(){l.parentNode.insertBefore(d,n?l:l.nextSibling)});var f=function(e){for(var t=d.href,n=u.length;n--;)if(u[n].href===t)return e();setTimeout(function(){f(e)})};return d.addEventListener&&d.addEventListener("load",a),d.onloadcssdefined=f,f(a),d};"undefined"!=typeof exports?exports.loadCSS=t:e.loadCSS=t}("undefined"!=typeof global?global:this),function(e){if(e.loadCSS){var t=loadCSS.relpreload={};if(t.support=function(){try{return e.document.createElement("link").relList.supports("preload")}catch(e){return!1}},t.poly=function(){for(var t=e.document.getElementsByTagName("link"),n=0;n<t.length;n++){var r=t[n];"preload"===r.rel&&"style"===r.getAttribute("as")&&(e.loadCSS(r.href,r,r.getAttribute("media")),r.rel=null)}},!t.support()){t.poly();var n=e.setInterval(t.poly,300);e.addEventListener&&e.addEventListener("load",function(){t.poly(),e.clearInterval(n)}),e.attachEvent&&e.attachEvent("onload",function(){e.clearInterval(n)})}}}(this);</script>
 
+        <!--Invisible recaptcha script-->
         <script src='https://www.google.com/recaptcha/api.js'></script>
+        <script>
+        function onSubmit(token) {
+            document.getElementById("demo-form").submit();
+        }
+        </script>
 
+        <!--Icon-->
         <link rel="icon" sizes="192x192" href="img/jffc-logo/icon.png">
     </head>
 
@@ -1311,6 +1326,7 @@
                             data-callback="onSubmit" 
                             data-badge="inline" >Send</button>
                     </div>
+                    <input name="token" type="hidden" value="<?php echo hash_hmac('sha256', '/form-handler.php', $_SESSION['token_key']) ?>">
                 </form>
             </div>
         </div>
